2fa5ebb1ddb849b0ae0b01aab79ea7ff489a54bb
Phillip Dyb-Sandnes <phillipdyb@Phillips-MacBook-Air-2.local>
2025-10-03 12:39:23 +0200

    Svelte initial

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<h2>Heading 2 for testing</h2>
+<h3>Heading 3 for testing</h3>
+<h4>Heading 4 for testing</h4>
+<h5>Heading 5 for testing</h5>
+<h6>Heading 6 va tydligvist det "høyeste"</h6>
+<p>Tekst tihi</p>
+<img src=https://c.tenor.com/yry9zYk-JwoAAAAC/tenor.gif />
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
